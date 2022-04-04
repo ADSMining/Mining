96691f5c2781cadfc03b5d0cdbab76ab176fa9f9
seth9022 <72393714+seth9022@users.noreply.github.com>
2022-04-04 23:44:20 +0100

    Lithium working for BHP
    Need to re pull comodity data to 2000/01/01, or if not available simply ignore EBITDA up to that point

---
+++ Notebooks/Datasets/Commodity_price_dataset/WranglerFunctions.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+def format_date(df, column_name, format_string):
+    err = 0
+    for i in range(len(df)):
+        try:
+            df.loc[i, column_name] = pd.to_datetime(df[column_name][i], format = format_string)
+        except ValueError:
+            err += 1
+    return err
+
+def Inflation_Unemployment_Wrangler(df):
+    date_err = format_date(df, 'Date', '%d/%m/%Y')
+    df = df.iloc[::-1]
+    df = df.reset_index(drop = True)
+    return df, date_err
+
+def F3Metl_Comp_Wrangler(data):
+    # Delete the first row (full of null values)
+    df = data.drop(labels=0)
+
+    # Delete the index column since the date is the index
+    df.rename(columns={'Unnamed: 0': 'Dates'}, inplace=True)
+    df.set_index('Dates', inplace=True)
+
+
+    # Rename the column headers
+    df = df.drop(labels='Dates')
+    df.columns = pd.MultiIndex.from_product([['RIO LN Equity', 'GLEN LN Equity', 'AAL LN Equity', 'ANTO LN Equity', 'EVR LN Equity', 'BHP LN Equity'], ['PX_OPEN', 'PX_HIGH', 'PX_LOW', 'PX_LAST', 'PX_VOLUME', 'EBITDA','PE_RATIO']])
+
+    # Delete rows without dates
+    df = df.drop(labels='#NAME?')
+
+    # Reformat dates
+    df.index = pd.to_datetime(df.index, format='%d/%m/%Y')
+
+    # Drop all BHP LN Equity data as it is incomplete
+    # However, the EBITDA data might be useful
+    # df.drop(columns= [('BHP LN Equity', y) for y in ['PX_OPEN', 'PX_HIGH', 'PX_LOW', 'PX_LAST', 'PX_VOLUME', 'EBITDA','PE_RATIO']], inplace=True)
+
+    # Change numbers to floats/ints (fields with NaN values are floats by default)
+    df = df.apply(pd.to_numeric)
+    return df
