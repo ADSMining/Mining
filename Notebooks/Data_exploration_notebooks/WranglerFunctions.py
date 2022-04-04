4ae1f520f04f1d7908f04f8942112fb0de17e0e8
aadithp <ci19639@bristol.ac.uk>
2022-04-04 21:20:00 +0100

    Modelling with ARIMA
    Started working on modelling the EBITDA and operating costs using ARIMA.

--- Notebooks/Data_exploration_notebooks/WranglerFunctions.py
+++ Notebooks/Data_exploration_notebooks/WranglerFunctions.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
 
 def format_date(df, column_name, format_string):
     err = 0
@@ -40,4 +44,59 @@ def F3Metl_Comp_Wrangler(data):
 
     # Change numbers to floats/ints (fields with NaN values are floats by default)
     df = df.apply(pd.to_numeric)
+    return df
+
+def CostsWrangler(df, start_str):
+    #Setting columns as the dates
+    df.columns = df.loc[3].values
+    #Getting rid of spaces in front of string
+    df['12 Months Ending'] = df['12 Months Ending'].str.strip()
+    #Removing nan columns
+    df = df.loc[:, df.columns.notna()]
+    
+    # Removing unneccessary rows
+    df = df.drop([0, 1, 2, 3, len(df) - 1], axis = 0)
+    df = df.reset_index(drop = True)
+    
+    # Get revenue index
+    rev_idx = df['12 Months Ending'][df['12 Months Ending'] == start_str].index[0]
+    
+    # Some float values have commas separating thousands - creates problems when converting to float
+    df = df.replace(',','', regex=True)
+    
+    # Replace the + and - characters
+    df = df.replace('\+ ','', regex=True)
+    df = df.replace('- ','', regex=True)
+    
+    # Putting dates as rows now
+    df = df.T
+    
+    # Columns as countries
+    df.columns = df.loc['12 Months Ending']
+    
+    # Only selecting relevant columns
+    df = df.iloc[:, rev_idx:]
+    
+    df = df.drop(['12 Months Ending'], axis = 0)
+    
+    # Converting values to float
+#     for column in df.columns:
+    # df.columns
+    df['Operating Expenses'] = df['Operating Expenses'].astype(float)    
+                
+    # Missing data as 0.0 - not 100% necessary
+    df = df.replace(np.nan, 0.0)
+    
+    # Date as datetime format
+    df = df.reset_index()
+    df = df.rename(columns = {"index" : "Date"})
+    
+    print(format_date(df, 'Date', '%m/%d/%Y'))
+    df.index = df['Date']
+    
+    df = df.drop('Date', axis = 1)
+    
+#     Only using data from 2010    
+    df = df[(df.index > '2010-01-01') & (df.index <'2022-01-01')]
+    
     return df
